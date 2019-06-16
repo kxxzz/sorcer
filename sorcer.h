da74b55468a846aa44feb2da39817f0d7ff4ddbf
www <www@www.com>
2019-06-16 16:23:48 +0800

    x

--- sorcer.h
+++ sorcer.h
@@ -1,43 +1,27 @@
 #pragma once
 
 
-#include <vec.h>
-#include <apnum.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
 
-typedef enum SORCER_ValueType
-{
-    SORCER_ValueType_UintC = 0,
-    SORCER_ValueType_IntC,
-    SORCER_ValueType_IntAP,
-    SORCER_ValueType_RatAP,
-    SORCER_ValueType_Src,
-    SORCER_ValueType_Block,
-    SORCER_ValueType_Array,
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long long s64;
 
-    SORCER_NumValueTypes
-} SORCER_ValueType;
-
-typedef struct SORCER_Value
-{
-    SORCER_ValueType type;
-    union
-    {
-        uintptr_t ui;
-        intptr_t i;
-        APNUM_int* intAP;
-        APNUM_rat* ratAP;
-        struct SORCER_Block* blk;
-        struct SORCER_Array* ary;
-    } as;
-} SORCER_Value;
-
-typedef vec_t(struct SORCER_Value) SORCER_ValueVec;
+typedef float f32;
+typedef double f64;
 
 
 
 typedef struct SORCER_Context SORCER_Context;
+
 
 SORCER_Context* SORCER_ctxNew(void);
 void SORCER_ctxFree(SORCER_Context* ctx);
---
+++ sorcer_a.h
@@ -0,0 +1,115 @@
+#pragma once
+
+
+
+#include "sorcer.h"
+
+
+#include <vec.h>
+
+
+
+
+#ifdef ARYLEN
+# undef ARYLEN
+#endif
+#define ARYLEN(a) (sizeof(a) / sizeof((a)[0]))
+
+
+
+
+#ifdef max
+# undef max
+#endif
+#ifdef min
+# undef min
+#endif
+#define max(a,b) ((a) > (b) ? (a) : (b))
+#define min(a,b) ((a) < (b) ? (a) : (b))
+
+
+
+
+#define zalloc(sz) calloc(1, sz)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
