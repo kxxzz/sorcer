0c0e2e63795350333448eeb7a3a33736a25b6a15
www <www@www.com>
2019-06-22 16:59:54 +0800

    x

--- sorcer.h
+++ sorcer.h
@@ -28,13 +28,13 @@ void SORCER_ctxFree(SORCER_Context* ctx);
 
 
 
-
 typedef struct SORCER_Block { u32 id; } SORCER_Block;
-typedef struct SORCER_Var { u32 id; } SORCER_Var;
-typedef struct SORCER_Step { u32 id; } SORCER_Step;
-
 static SORCER_Block SORCER_Block_Invalid = { (u32)-1 };
+
+typedef struct SORCER_Var { u32 id; } SORCER_Var;
 static SORCER_Var SORCER_Var_Invalid = { (u32)-1 };
+
+typedef struct SORCER_Step { u32 id; } SORCER_Step;
 static SORCER_Step SORCER_Step_Invalid = { (u32)-1 };
 
 
@@ -54,43 +54,11 @@ typedef struct SORCER_Cell
 
 
 
-
-
 u32 SORCER_dsSize(SORCER_Context* ctx);
 const SORCER_Cell* SORCER_dsBase(SORCER_Context* ctx);
 
 void SORCER_dsPush(SORCER_Context* ctx, const SORCER_Cell* x);
 void SORCER_dsPop(SORCER_Context* ctx, u32 n, SORCER_Cell* out);
-
-
-
-
-
-u32 SORCER_ctxBlocksTotal(SORCER_Context* ctx);
-void SORCER_ctxBlocksRollback(SORCER_Context* ctx, u32 n);
-
-
-
-
-
-SORCER_Block SORCER_blockNew(SORCER_Context* ctx);
-
-SORCER_Var SORCER_blockAddInstPopVar(SORCER_Context* ctx, SORCER_Block blk);
-
-void SORCER_blockAddInstPushCell(SORCER_Context* ctx, SORCER_Block blk, const SORCER_Cell* x);
-void SORCER_blockAddInstPushVar(SORCER_Context* ctx, SORCER_Block blk, SORCER_Var v);
-void SORCER_blockAddInstPushBlock(SORCER_Context* ctx, SORCER_Block blk, SORCER_Block b);
-void SORCER_blockAddInstStep(SORCER_Context* ctx, SORCER_Block blk, SORCER_Step step);
-void SORCER_blockAddInstCall(SORCER_Context* ctx, SORCER_Block blk, SORCER_Block callee);
-void SORCER_blockAddInstApply(SORCER_Context* ctx, SORCER_Block blk);
-void SORCER_blockAddInstIfte(SORCER_Context* ctx, SORCER_Block blk);
-
-void SORCER_blockAddInlineBlock(SORCER_Context* ctx, SORCER_Block blk, SORCER_Block ib);
-
-void SORCER_blockAddPatIfteCT(SORCER_Context* ctx, SORCER_Block blk, SORCER_Block onTrue, SORCER_Block onFalse);
-
-
-
 
 
 
@@ -105,6 +73,30 @@ typedef struct SORCER_StepInfo
 
 SORCER_Step SORCER_stepNew(SORCER_Context* ctx, const SORCER_StepInfo* info);
 void SORCER_step(SORCER_Context* ctx, SORCER_Step step);
+
+
+
+u32 SORCER_ctxBlocksTotal(SORCER_Context* ctx);
+void SORCER_ctxBlocksRollback(SORCER_Context* ctx, u32 n);
+
+SORCER_Block SORCER_blockNew(SORCER_Context* ctx);
+
+
+
+
+SORCER_Var SORCER_blockAddInstPopVar(SORCER_Context* ctx, SORCER_Block blk);
+
+void SORCER_blockAddInstPushCell(SORCER_Context* ctx, SORCER_Block blk, const SORCER_Cell* x);
+void SORCER_blockAddInstPushVar(SORCER_Context* ctx, SORCER_Block blk, SORCER_Var v);
+void SORCER_blockAddInstPushBlock(SORCER_Context* ctx, SORCER_Block blk, SORCER_Block b);
+void SORCER_blockAddInstStep(SORCER_Context* ctx, SORCER_Block blk, SORCER_Step step);
+void SORCER_blockAddInstCall(SORCER_Context* ctx, SORCER_Block blk, SORCER_Block callee);
+void SORCER_blockAddInstApply(SORCER_Context* ctx, SORCER_Block blk);
+void SORCER_blockAddInstIfte(SORCER_Context* ctx, SORCER_Block blk);
+
+void SORCER_blockAddInlineBlock(SORCER_Context* ctx, SORCER_Block blk, SORCER_Block ib);
+
+void SORCER_blockAddPatIfteCT(SORCER_Context* ctx, SORCER_Block blk, SORCER_Block onTrue, SORCER_Block onFalse);
 
 
 
