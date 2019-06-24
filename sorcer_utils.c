f538c9a627800d5397b2f198bfa00625b12b4f9d
www <www@www.com>
2019-06-24 08:38:43 +0800

    x

--- sorcer_utils.c
+++ sorcer_utils.c
@@ -9,14 +9,17 @@
 void SORCER_dsFprint(FILE* f, SORCER_Context* ctx)
 {
     const SORCER_Cell* base = SORCER_dsBase(ctx);
-    char buf[1024] = { 0 };
+    vec_char buf[1] = { 0 };
     for (u32 i = 0; i < SORCER_dsSize(ctx); ++i)
     {
-        u32 n = SORCER_cellToStr(buf, sizeof(buf), ctx, base + i);
-        buf[n] = 0;
-        fprintf(f, "%s", buf);
+        u32 n = SORCER_cellToStr(NULL, 0, ctx, base + i);
+        vec_resize(buf, n + 1);
+        n = SORCER_cellToStr(buf->data, buf->length, ctx, base + i);
+        buf->data[n] = 0;
+        fprintf(f, "%s", buf->data);
         fprintf(f, "\n");
     }
+    vec_free(buf);
 }
 
 
